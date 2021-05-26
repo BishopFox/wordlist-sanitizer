68acf07f505b576a7ea681dc4a152ac3f9a420cd
gaberust <gabe.rust@outlook.com>
2021-05-25 21:01:22 -0400

    Add wordlist and command-line argument parsing

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 func main() {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	defaultBadPath := filepath.Join(filepath.Dir(ex), "bad-words.txt")
+
+	var inPath string
+	flag.StringVar(&inPath, "path", ".", "The path of the target file or directory.\n"+
+		"May also be passed after all flags as a positional argument.")
+
+	var outPath string
+	flag.StringVar(&outPath, "out", ".", "The output directory.")
+
+	var badPath string
+	flag.StringVar(&badPath, "bad", defaultBadPath, "The list of words to be stripped.")
+
+	flag.Parse()
+
+	fmt.Println("path:", inPath)
+	fmt.Println("out:", outPath)
+	fmt.Println("bad:", badPath)
 
 }
